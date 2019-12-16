3dc9169857aad07d21de1221fed7adfcbd1c38e8
Lương Văn Chung Đức - CMC Global DU2 <lvcduc@ldap.cmcglobal.com.vn>
2019-12-16 15:41:46 +0700

    duc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { DashbroadComponent } from './Components/Admin/dashbroad/dashbroad.component';
+import { LoginComponent } from './Components/Admin/login/login.component';
+import { RegisterComponent } from './Components/Admin/register/register.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: DashbroadComponent },
+  { path: 'register', component: RegisterComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbroadComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // other imports here
   ],
   providers: [],
   bootstrap: [AppComponent]
